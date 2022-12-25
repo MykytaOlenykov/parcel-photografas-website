3e12d35583b613b1a2b7aee6dc63f81860014899
MykytaOlenykov <115184331+MykytaOlenykov@users.noreply.github.com>
2022-12-25 17:05:08 +0000

    Deploying to gh-pages from @ MykytaOlenykov/parcel-photografas-website@dc9d02e9807f205caba52f8b09893fa23e2e72e5 🚀

--- index.029e369d.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector(".open-menu"),t=document.querySelector(".close-menu"),c=document.querySelector(".mobile-menu");function n(){c.classList.toggle("active"),document.body.classList.toggle("lock"),window.matchMedia("(min-width: 768px)").addEventListener("change",(function(e){e.matches&&(c.classList.remove("active"),document.body.classList.remove("lock"))}))}e.addEventListener("click",n),t.addEventListener("click",n)}();
-//# sourceMappingURL=index.029e369d.js.map
